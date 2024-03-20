f2c9c698fd259e2219f4ce6f28f97c4110f78c84
gor241 <nuriev241@mail.ru>
2024-03-20 11:18:32 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import "./index.css";
+import React from 'react';
+import App from "./App";
+import * as ReactDOMClient from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+const root = ReactDOMClient.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    {/* <BrowserRouter> */}
+      <App />
+    {/* </BrowserRouter> */}
+  </React.StrictMode>
+);
